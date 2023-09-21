a02597a492af94ad72f41a7706e26363d1795f39
Olu433 <calebawe0@gmail.com>
2023-09-21 15:54:43 -0700

    header file

--- shell.h
+++ shell.h
@@ -1,34 +1,53 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-/*standard library*/
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
 
-/*main shell functions*/
-int start_shell(int ac, char **av, char **env);
-char *get_input(char **av);
-char **input_tokenizer(char *input, char **av);
-int tokens_number(char *input);
-void execute_commands(char **tokens, char **av, char **env);
-int get_accessable_path(char **tokens, char **env);
-char *_getenv(char **env, char *command);
-void print_env(char **env);
-void free_arr(char **arr);
-int custom_commands(char **tokens, char **env);
-void exit_shell(char **tokens);
+/* Utility Functions */
+void freeArray(char **arr);
+void printError(char *name, char *cmd, int index);
+void printExitError(char *name, char *cmd, int index);
+char *_intToAscii();
+void reverseStr(char *string, int length);
+int isPositive(char *str);
+int _atoi(char *str);
 
-/*utility functions*/
-int _strlen(char *line);
-int _strcpy(char *dest, char *src);
-int _strcmp(char *s1, char *s2, size_t len);
-char *_strcat(char *dest, const char *src);
-void _print(int status, char **av, char *message);
-void print_error(int status, char **av, char *command);
+/* Utility Functions (utils2) */
+char *_getEnvironment(char *variable, char **environment);
+char **tokenize(char *line);
+char *readline(void);
 
-#endif /*SHELL_H*/
+/*  String Manipulation Functions */
+int _strlen(char *string);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strdup(const char *str);
+int _strcmp(char *s1, char *s2);
+
+/* Execute Function */
+int _execute(char **cmd, char **argv, char **environment, int index);
+
+/* Handlers */
+char *_handlePath(char *cmd, char **environment);
+void handleExit(char **command, char **argv, int *status, int index);
+void handleBuiltIn(char **command, char **argv, int *status, int index, char **environment);
+void handleEnv(char **command, int *status, char **environment);
+
+/* Custom StringTokenizer */
+char *myStrtok(char *str, const char *delim);
+
+/* Custom Getline Function */
+ssize_t myGetline(char **linePtr, size_t *n, FILE *stream);
+
+/* Check Built-In Command */
+int isBuiltIn(char *command);
+
+#endif /* SHELL_H */
